5c42d2b284a193683bf9c8007ea22a9a0befbd35
jonreesman <jreesman1@mac.com>
2022-05-02 19:15:12 -0500

    Separated kafkaTopics by app.

--- docker/kafka/createTopics.sh
+++ docker/kafka/createTopics.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # kafka-demo topics
+docker-compose exec kafka kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 10 --topic demo-command
+docker-compose exec kafka kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 10 --topic demo-domain
+docker-compose exec kafka kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 10 --topic demo-public
 docker-compose exec kafka kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 10 --topic secondary-public
